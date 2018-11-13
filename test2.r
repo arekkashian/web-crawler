bdab3fdc442ee9001d693d37a48e550f61f3af5d
Arek Kashian <arek.kashian@gmail.com>
2018-11-13 14:56:03 -0600

    Update test2.r

--- test2.r
+++ test2.r
@@ -1,14 +1,18 @@
 library(rvest)
 library(httr)
-library(RJSONIO)
 library(jsonlite)
 
 template="https://www.googleapis.com/customsearch/v1?key=AIzaSyBX0wXkc7B8a7xRamGWY0SUHBO3zQjey1g&cx=008545004345632418708:amsec8dvzdk&q="
 
-query="iron"
+repeat {
 
-result = content(GET(paste(template,query,sep='')))
+  query <- readline(prompt="Enter a query, or exit to quit: ")
 
-#write(toJSON(result), "result.json")
+  if (query == "exit") break;
 
-print(result[['items']][[5]])
+  result = content(GET(paste(template,query,sep='')))
+
+  #write(toJSON(result), "result.json")
+
+  print(result[['items']][[6]][['title']])
+}
